858e0dff9bd6b5195847fe7e7bc5d8db7ef6bff4
John Leal <jlealgom@unal.edu.co>
2025-12-31 15:35:59 -0500

    Mejorando las salidas 01

--- javascripts/mathjax-config.js
+++ javascripts/mathjax-config.js
@@ -1,5 +1,5 @@
 window.MathJax = {
   loader: {load: ['[tex]/boldsymbol', '[tex]/ams']},
-  tex: { packages: {'[+]': ['boldsymbol', 'ams']}, inlineMath: [["\(", "\)"], ["$", "$"]], displayMath: [["\[", "\]"], ["1842700", "1842700"]] },
+  tex: { packages: {'[+]': ['boldsymbol', 'ams']}, inlineMath: [["\(", "\)"], ["$", "$"]], displayMath: [["\[", "\]"], ["2904674", "2904674"]] },
   options: { processHtmlClass: "arithmatex|imagen-caption" }
 };
